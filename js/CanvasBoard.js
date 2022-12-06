aedc6b11c902080814228e61a96451ca1f83f402
hadeelgamal <hadeel.abdelkarim@gmail.com>
2022-12-06 13:18:55 +0200

    change directions in Canvasboard

--- js/CanvasBoard.js
+++ js/CanvasBoard.js
@@ -13,11 +13,20 @@ const passengersArr = [
 const backgroundImage = new Image();
 const tramImage = new Image();
 
+// // Horizontal velocity
+// let dx = 30;
+// // Vertical velocity
+// let dy = 0;
 class CanvasBoard {
   constructor() {
     this.tramImg = "images/tram.png";
     this.backgroundImg = "images/cairo-map.png";
-
+    this.dx = 30;
+    this.dy = 0;
+    // this.changing_direction = false;
+    this.width = canvas.width;
+    this.height = canvas.height;
+    console.log(this.width);
   }
 
   drawBackgroundImage() {
@@ -34,19 +43,14 @@ class CanvasBoard {
     tramImage.src = this.tramImg;
     tramImage.addEventListener("load", () => {
       tramCars.forEach((element) => {
-        ctx.drawImage(tramImage, element.x, element.y, 50, 50);
+        ctx.drawImage(tramImage, element.x, element.y, 50, 30);
       });
     });
   }
 
   moveTram(tramCars) {
-    // Horizontal velocity
-    let dx = 30;
-    // Vertical velocity
-    let dy = 0;
-
     console.log(tramCars);
-    const head = { x: tramCars[0].x + dx, y: tramCars[0].y + dy };
+    const head = { x: tramCars[0].x + this.dx, y: tramCars[0].y + this.dy };
     tramCars.unshift(head);
     tramCars.pop();
 
@@ -55,41 +59,99 @@ class CanvasBoard {
   }
   addRandomPassenger(currentPassenger) {
     const randoNumber = Math.floor(Math.random() * 10);
-    const imageUrls = passengersArr[randoNumber];
-
+    const imageUrl = passengersArr[randoNumber];
 
     const randoX = Math.floor(Math.random() * 300);
     const randoY = Math.floor(Math.random() * 300);
 
-
-    currentPassenger.imagePath = imageUrls
+    currentPassenger.imagePath = imageUrl;
     currentPassenger.x = randoX;
     currentPassenger.y = randoY;
-    
+
+    this.drawPassenger(currentPassenger);
   }
   drawPassenger(currentPassenger) {
     const passengerImage = new Image();
     passengerImage.src = currentPassenger.imagePath;
 
-    passengerImage.addEventListener("load", () => 
-      ctx.drawImage(
-        passengerImage,
-        currentPassenger.randoX,
-        currentPassenger.randoY,
-        40,
-        40
-      )
+    ctx.drawImage(
+      passengerImage,
+      currentPassenger.x,
+      currentPassenger.y,
+      10,
+      40
     );
-    console.log(passengerImage.src);
-  
   }
 
-  gameEnded() {
-    // checks if tram has collided with its body OR collided with the canvas
+  moveLeft() {
+    this.dx -= 10;
+    this.dy = 0;
+    console.log("moving left:", this.dx);
+  }
+  moveRight() {
+    this.dx += 10;
+    this.dy = 0;
+    console.log("moving right:", this.dx);
+  }
+  moveUp() {
+    this.dx = 0;
+    this.dy -= 10;
+
+    console.log("moving up:", this.dx);
+  }
+  moveDown() {
+    this.dx = 0;
+    this.dy += 10;
+
+    console.log("moving down:", this.dx);
   }
 
-  changeDirections() {
-    // Add document event listener on keydown
-    // switch case left right top bottom arrow
+  // changeDirections(event) {
+
+  //     const LEFT_KEY = 37;
+  //     const RIGHT_KEY = 39;
+  //     const UP_KEY = 38;
+  //     const DOWN_KEY = 40;
+
+  //     // Prevent the snake from reversing
+
+  //     if (changing_direction) return;
+  //     changing_direction = true;
+  //     const keyPressed = event.keyCode;
+  //     const goingUp = this.dy === -10;
+  //     const goingDown = this.dy === 10;
+  //     const goingRight = this.dx === 10;
+  //     const goingLeft = this.dx === -10;
+  //     if (keyPressed === LEFT_KEY && !goingRight) {
+  //       this.dx = -10;
+  //       this.dy = 0;
+  //     }
+  //     if (keyPressed === UP_KEY && !goingDown) {
+  //       this.dx = 0;
+  //       this.dy = -10;
+  //     }
+  //     if (keyPressed === RIGHT_KEY && !goingLeft) {
+  //       this.dx = 10;
+  //       this.dy = 0;
+  //     }
+  //     if (keyPressed === DOWN_KEY && !goingUp) {
+  //       this.dx = 0;
+  //       this.dy = 10;
+  //     }
+
+  //   console.log(event);
+
+  // }
+
+  gameEnded(tramCars) {
+    for (let i = 4; i < tramCars.length; i++) {
+      if (tramCars[i].x === tramCars[0].x && tramCars[i].y === tramCars[0].y)
+        return true;
+    }
+    const hitLeftWall = tramCars[0].x < 0;
+    const hitRightWall = tramCars[0].x > this.width - 10;
+    const hitToptWall = tramCars[0].y < 0;
+    const hitBottomWall = tramCars[0].y > this.height - 10;
+    return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall;
   }
 }
--- js/index.js
+++ js/index.js
@@ -4,24 +4,37 @@ const ctx = canvas.getContext("2d");
 const currentBoard = new CanvasBoard();
 const cairoTram = new Tram();
 
-
 class Passenger {
-    constructor() {
-        this.imagePath= "";
-        this.x = 0;
-        this.y = 0;
-    }
-    addGen() {
-        // adds a tram car to the array tramCars
-      }
+  constructor() {
+    this.imagePath = "";
+    this.x = 0;
+    this.y = 0;
+    this.status = false;
+  }
 }
-
 const currentPassenger = new Passenger();
 
+document.addEventListener("keydown", (event) => {
+  console.log("event keyCode", event.keyCode);
+  switch (event.keyCode) {
+    case 37:
+      currentBoard.moveLeft();
+      break;
+    case 39:
+      currentBoard.moveRight();
+      break;
+    case 38:
+      currentBoard.moveUp();
+      break;
+    case 40:
+      currentBoard.moveDown();
+      break;
+  }
+});
 
 document.getElementById("start-button").onclick = () => {
   startGame();
-  setInterval(updateGame, 1000);
+  setInterval(updateGame, 2200);
 };
 
 function startGame() {
@@ -32,17 +45,14 @@ function startGame() {
   currentBoard.drawTramCar(cairoTram.tramCars);
   currentBoard.addRandomPassenger(currentPassenger);
 
-  // call the moveTram method
-  // call the addRandomPassenger
+  // document.addEventListener("keydown", currentBoard.changeDirections);
 }
 function updateGame() {
+  if (currentBoard.gameEnded(cairoTram.tramCars)) return;
+  changing_direction = false;
   ctx.clearRect(0, 0, 500, 500);
   currentBoard.reDrawBackground();
   currentBoard.drawTramCar(cairoTram.tramCars);
   currentBoard.moveTram(cairoTram.tramCars);
   currentBoard.drawPassenger(currentPassenger);
 }
-//   function updateCanvas() {
-//
-
-// }
